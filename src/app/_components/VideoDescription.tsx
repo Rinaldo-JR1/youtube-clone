33b34f0bf99a5336db9baad01428b0ba8c6c5f51
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-06-24 22:09:39 -0300

    Description

---
+++ src/app/_components/VideoDescription.tsx
@@ -0,0 +1,12 @@
+const VideoDescription = () => {
+  return (
+    <div className="w-3/5 mt-4 text-[#515151]">
+      Inicie uma aventura imersiva no mundo do Front-end com HTML e CSS. Para
+      isso, criaremos uma landing page com a temática da série Stranger Things,
+      a qual contará com a implementação de um theme switcher (light e dark)
+      para explorarmos os mistérios do mundo invertido.
+    </div>
+  );
+};
+
+export default VideoDescription;
