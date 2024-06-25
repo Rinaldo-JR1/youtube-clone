65d683994d5f76a209de441e6416ccd59b805147
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-06-24 21:37:46 -0300

    Trocar para react icons

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,3 +1,6 @@
+import { BiVideo } from "react-icons/bi";
+import { TfiMenu, TfiSearch, TfiBell } from "react-icons/tfi";
+
 /* eslint-disable @next/next/no-img-element */
 export const Navbar = () => {
   return (
@@ -5,7 +8,7 @@ export const Navbar = () => {
       <div className="w-full h-24 flex justify-between">
         <div className="flex justify-center text-center gap-4">
           <div>
-            <img src="/assets/menu.png" alt="menu" className="w-full" />
+            <TfiMenu />
           </div>
           <div>
             <img src="/assets/youtube.png" alt="logo" className="w-full" />
@@ -15,14 +18,19 @@ export const Navbar = () => {
           <input className="w-full h-full" />
           <div className="w-16 flex justify-center h-full bg-gray-200 border-2 border-gray-400">
             <button>
-              <img src="/assets/search.png" alt="search" />
+              {/* <img src="/assets/search.png" alt="search" /> */}
+              <TfiSearch />
             </button>
           </div>
         </div>
-        <div className="flex gap-4">
-          <img src="/assets/video.png" alt="video" className="h-12" />
-          <img src="/assets/bell.png" alt="bell" className="h-12" />
-          <img src="/assets/eu.png" alt="eu" className="h-12 rounded-full text-center" />
+        <div className="flex justify-center align-middle  gap-4">
+          <BiVideo className="h-12" size={30}  />
+          <TfiBell className="h-12"  size={30}/>
+          <img
+            src="/assets/eu.png"
+            alt="eu"
+            className="h-12 rounded-full text-center"
+          />
         </div>
       </div>
     </header>
